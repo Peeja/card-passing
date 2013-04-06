77e8cd8ed07d7ed72a35e5608a7331704e1bdfba
Peter Jaros <peter.a.jaros@gmail.com>
2013-04-06 15:43:57 -0400

    Extract card into data structure.

--- cards.rb
+++ cards.rb
@@ -1,21 +1,30 @@
 require 'prawn'
 require "prawn/measurement_extensions"
 
+cards = [
+  {
+    name: "Loose Stone",
+    description: "Dig the card below for free."
+  }
+]
+
 Prawn::Document.generate("cards.pdf", page_size: [2.5.in, 3.5.in]) do
   font_families.update("Skia" => {
     :normal => "/Library/Fonts/Skia.ttf",
   })
   font "Skia"
 
-  text_box "Loose Stone",
-           size: 12,
-           at: [0, margin_box.top],
-           width: margin_box.width,
-           align: :center
+  cards.each do |card|
+    text_box card.fetch(:name),
+             size: 12,
+             at: [0, margin_box.top],
+             width: margin_box.width,
+             align: :center
 
-  text_box "Dig the card below for free.",
-           size: 10,
-           at: [0, margin_box.top-100],
-           width: margin_box.width,
-           align: :center
+    text_box card.fetch(:description),
+             size: 10,
+             at: [0, margin_box.top-100],
+             width: margin_box.width,
+             align: :center
+  end
 end
